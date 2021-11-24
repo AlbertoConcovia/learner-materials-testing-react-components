7156e3677552c036fbd5644291dcbee29ab8fe09
albertoconcovia <concovia@gmail.com>
2021-11-24 00:48:05 +0000

    Activity 5 - Validation - const WhatIs2plus2 = ({ whatIs2plus2, onChangeWhatIs2plus2 }) => { input validated

--- src/components/WhatIs2plus2.js
+++ src/components/WhatIs2plus2.js
@@ -1,11 +1,36 @@
-const WhatIs2plus2 = ({whatIs2plus2, onChangeWhatIs2plus2}) => (
-  <>
-    <label htmlFor="fwhatis2plus2">What is 2+2? </label>
-    <select name="fwhatis2plus2" id="fwhatis2plus2" value={whatIs2plus2} onChange={onChangeWhatIs2plus2}>
-      <option value="not4">Not 4</option>
-      <option value="4">4</option>
-    </select>
-  </>
-);
+import { useState } from "react";
+import ErrorMessage from "./ErrorMessage";
+
+const WhatIs2plus2 = ({ whatIs2plus2, onChangeWhatIs2plus2 }) => {
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const validate = (value) => {
+    // What is 2 + 2: "4" must be selected. Selecting "Not 4" should display an error.
+    alert(`value ${value}`);
+    return value === "4" ? " " : "error: Human please go back to school - You Must choose 4";
+  };
+
+  return (
+    <div>
+      <label htmlFor="fwhatis2plus2">What is 2+2? </label>
+
+      <select
+        name="fwhatis2plus2"
+        id="fwhatis2plus2"
+        type="text"
+        value={whatIs2plus2}
+        onChange={(e) => {
+          const errorMessage = validate(e.target.value);
+          setErrorMessage(errorMessage);
+          onChangeWhatIs2plus2(e);
+        }}
+      >
+        <option value="not4">Not 4</option>
+        <option value="4">4</option>
+      </select>
+      <ErrorMessage errorMessage={errorMessage} />
+    </div>
+  );
+};
 
 export default WhatIs2plus2;
