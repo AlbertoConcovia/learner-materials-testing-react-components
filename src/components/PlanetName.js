60dbeb8729aebcfa05249a1d9af3786d4e7d062e
albertoconcovia <concovia@gmail.com>
2021-11-24 00:19:25 +0000

    Activity 5 - Validation - PlanetName input validated

--- src/components/PlanetName.js
+++ src/components/PlanetName.js
@@ -1,8 +1,36 @@
-const PlanetName = ({planetName, onChangePlanetName}) => (
-	<div>
+import {useState} from "react";
+import ErrorMessage from "./ErrorMessage";
+
+const PlanetName = ( {planetName, onChangePlanetName}) => {
+	
+	const [ errorMessage, setErrorMessage ] = useState('');
+    
+    const validate = (value) => {
+		// Planet Name: Must be between 2 and 49 characters. No special characters.
+		// Numbers are allowed.
+        const regex = /^[a-zA-Z0-9]{2,49}$/;
+        return (regex.test(value)) ? " " : 'Must be between 2 and 49 characters. No numbers or special characters.';
+    }
+	
+    return(
+    <div>
+
 		<label htmlFor="planetname">Planet Name</label>
- 		<input type="text" id="planetname" value={planetName} onChange={onChangePlanetName}/>
-	</div>
+        <input
+            id='planetname'
+            type='text'
+            value={planetName}
+            onChange={(e) => {
+                const errorMessage = validate(e.target.value);
+                setErrorMessage(errorMessage);
+                onChangePlanetName(e);
+            }
+        }
+        />
+        <ErrorMessage errorMessage={errorMessage}/>
+    </div>
+
 );
+}
 
 export default PlanetName;
