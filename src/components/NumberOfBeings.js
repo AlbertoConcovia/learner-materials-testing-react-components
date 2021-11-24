12b16646c1e1a9771bc62e2e852799d23daf0888
albertoconcovia <concovia@gmail.com>
2021-11-24 00:33:38 +0000

    Activity 5 - Validation - NumberOfBeing input validated

--- src/components/NumberOfBeings.js
+++ src/components/NumberOfBeings.js
@@ -1,10 +1,34 @@
+import { useState } from "react";
+import ErrorMessage from "./ErrorMessage";
 
-const NumberOfBeings = ({numberOfBeings, onChangeNumberOfBeings}) => ( 
+const NumberOfBeings = ({ numberOfBeings, onChangeNumberOfBeings }) => {
+  const [errorMessage, setErrorMessage] = useState("");
+  
+  const validate = (value) => {
+    // Number of beings: Numbers ONLY. Must be at least 1000000000.
+    const regex = /^[0-9]{10,}$/;
+    return regex.test(value)
+      ? " "
+      : "Must be numbers ONLY and at least 1,000,000,000.";
+  };
+
+  return (
     <div>
       <label htmlFor="numberofbeings">Number of beings: </label>
-      <input id='numberofbeings' type="text" value={numberOfBeings} onChange={onChangeNumberOfBeings} />
-    </div> 
+      <input
+        id='numberofbeings'
+        type='text'
+        value={numberOfBeings}
+        onChange={(e) => {
+          const errorMessage = validate(e.target.value);
+          setErrorMessage(errorMessage);
+          onChangeNumberOfBeings(e);
+        }
+      }
+      />
+      <ErrorMessage errorMessage={errorMessage} />
+    </div>
   );
-
+};
 
 export default NumberOfBeings;
