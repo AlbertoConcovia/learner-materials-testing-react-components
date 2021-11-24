832793052d33870749155af7c12bb15eab34422f
albertoconcovia <concovia@gmail.com>
2021-11-24 01:17:52 +0000

    Activity 5 - Validation completed

--- src/components/ReasonForSparing.js
+++ src/components/ReasonForSparing.js
@@ -1,14 +1,36 @@
-const ReasonForSparing = ({reasonForSparing, setReasonForSparing}) => (
-  <>
-    <label htmlFor="freasonforsparing">Reason for sparing</label>
-    <textarea
-      id="reasonforsparing"
-      placeholder={reasonForSparing}
-      rows="5"
-      cols="40"
-      onChange={setReasonForSparing}
-    ></textarea>
-  </>
-);
+import { useState } from "react";
+import ErrorMessage from "./ErrorMessage";
+
+const ReasonForSparing = ({ reasonForSparing, onChangeReasonForSparing }) => {
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const validate = (value) => {
+    // Reason for sparing: Must be between 17 and 153 characters.
+    const regex = /^[ A-Za-z0-9_@./#&+-]{17,153}$/;
+    return (regex.test(value))
+      ? " "
+      : "Do not piss off the aliens - Reason for sparing: Must be between 17 and 153 characters.";
+  };
+
+  return (
+    <div>
+      <label htmlFor="reasonforsparing">Reason for sparing</label>
+
+      <textarea
+        id="reasonforsparing"
+        name="reasonforsparing"
+        rows="5"
+        cols="40"
+        value={reasonForSparing}
+        onChange={(e) => {
+          const errorMessage = validate(e.target.value);
+          setErrorMessage(errorMessage);
+          onChangeReasonForSparing(e);
+        }}
+      />
+      <ErrorMessage errorMessage={errorMessage} />
+    </div>
+  );
+};
 
 export default ReasonForSparing;
